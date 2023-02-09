f6c340532511ba5550c2ecda063bbd0eb30b25c7
CannotCreateUsername <peckerisawesome@outlook.com>
2023-02-09 13:31:24 -0800

    Speed Testing, Default Constraints, Remove Backwards driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/pp/SpeedTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/pp/SpeedTesting.java
@@ -6,12 +6,10 @@ import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.drive.AlignJunction;
+import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.GamepadHelper;
-import org.firstinspires.ftc.teamcode.drive.IntakeSlide2;
 import org.firstinspires.ftc.teamcode.drive.IntakeSlideSubsystem4;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
@@ -22,13 +20,14 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
  * exercise is to ascertain whether the localizer has been configured properly (note: the pure
  * encoder localizer heading may be significantly off if the track width has not been tuned).
  */
-@TeleOp(name="Odyssea Drive", group = "Linear Opmode")
-public class PowerPlayTeleOpServo extends LinearOpMode {
+@TeleOp(name="Speed Testing", group = "Linear Opmode")
+public class SpeedTesting extends LinearOpMode {
 
     private enum TurnState {
         STRAIGHT,
         LEFT,
         RIGHT,
+        BACKWARDS
     }
 
     TurnState turnState;
@@ -37,9 +36,16 @@ public class PowerPlayTeleOpServo extends LinearOpMode {
     private double LeftYInput;
     private double RightXInput;
 
+    private double MaxSpeedMultiplier = 1;
+
+    private TouchSensor sensorTouch1;
+    private TouchSensor sensorTouch2;
+
     @Override
     public void runOpMode() throws InterruptedException {
         turnState = TurnState.STRAIGHT;
+        sensorTouch1 = hardwareMap.get(TouchSensor.class, "touch1");
+        sensorTouch2 = hardwareMap.get(TouchSensor.class, "touch2");
 
         GamepadEx gamepadEx1 = new GamepadEx(gamepad1);
         GamepadEx gamepadEx2 = new GamepadEx(gamepad2);
@@ -49,69 +55,62 @@ public class PowerPlayTeleOpServo extends LinearOpMode {
         IntakeSlideSubsystem4 intakeSlide = new IntakeSlideSubsystem4();
         intakeSlide.init(hardwareMap);
 
-        // by default , use Drive Control #1
-
-        double leftStickMultiplierX, leftStickMultiplierY, rightStickMultiplierX;
-        GamepadHelper leftStickX = new GamepadHelper();
-        leftStickX.init();
-        GamepadHelper leftStickY = new GamepadHelper();
-        leftStickY.init();
-        GamepadHelper rightStickX = new GamepadHelper();
-        rightStickX.init();
-
-        AlignJunction alignStick = new AlignJunction();
-        alignStick.init(drive, hardwareMap);
 
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        //dsfadf
+
         waitForStart();
 
         while (opModeIsActive()) {
             Pose2d poseEstimate = drive.getPoseEstimate();
 
-            // drivebase control loop
-            leftStickMultiplierX = leftStickX.getGamepadStickRampingMultiplier(gamepad1.left_stick_x);
-            leftStickMultiplierY = leftStickY.getGamepadStickRampingMultiplier(gamepad1.left_stick_y);
-            rightStickMultiplierX = rightStickX.getGamepadStickRampingMultiplier(gamepad1.right_stick_x);
-            // alignMultiplierY = alignStick.getGamepadStickRampingMultiplier(gamepad1.left_stick_y);
-
-
             // keeps controls the same if robot is rotated 90 degrees in any direction
             switch (turnState) {
                 case STRAIGHT:
-                    LeftXInput = gamepad1.left_stick_x * leftStickMultiplierX * intakeSlide.dropOffMultiplier;
-                    LeftYInput = gamepad1.left_stick_y * leftStickMultiplierY * intakeSlide.dropOffMultiplier;
-                    RightXInput = gamepad1.right_stick_x * rightStickMultiplierX * intakeSlide.dropOffMultiplier;
-                    if (gamepadEx1.wasJustReleased(GamepadKeys.Button.X)) {
+                    LeftXInput = gamepad1.left_stick_x * MaxSpeedMultiplier * intakeSlide.dropOffMultiplier;
+                    LeftYInput = gamepad1.left_stick_y * MaxSpeedMultiplier * intakeSlide.dropOffMultiplier;
+                    RightXInput = gamepad1.right_stick_x * MaxSpeedMultiplier * intakeSlide.dropOffMultiplier;
+                    if (gamepadEx1.wasJustReleased(GamepadKeys.Button.DPAD_LEFT)) {
                         drive.turn(Math.toRadians(90));
                         turnState = TurnState.LEFT;
-                    } else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.Y)) {
+                    } else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.DPAD_RIGHT)) {
                         drive.turn(Math.toRadians(-90));
                         turnState = TurnState.RIGHT;
                     }
                     break;
                 case LEFT:
-                    LeftXInput = -gamepad1.left_stick_y * leftStickMultiplierY * intakeSlide.dropOffMultiplier;
-                    LeftYInput = gamepad1.left_stick_x * leftStickMultiplierX * intakeSlide.dropOffMultiplier;
-                    RightXInput = gamepad1.right_stick_x * rightStickMultiplierX * intakeSlide.dropOffMultiplier;
-                    if (gamepadEx1.wasJustReleased(GamepadKeys.Button.X)) {
+                    LeftXInput = -gamepad1.left_stick_y * MaxSpeedMultiplier * intakeSlide.dropOffMultiplier;
+                    LeftYInput = gamepad1.left_stick_x * MaxSpeedMultiplier * intakeSlide.dropOffMultiplier;
+                    RightXInput = gamepad1.right_stick_x * MaxSpeedMultiplier * intakeSlide.dropOffMultiplier;
+                    if (gamepadEx1.wasJustReleased(GamepadKeys.Button.DPAD_LEFT)) {
+                        drive.turn(Math.toRadians(90));
+                        turnState = TurnState.BACKWARDS;
+                    } else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.DPAD_RIGHT)) {
                         drive.turn(Math.toRadians(-90));
                         turnState = TurnState.STRAIGHT;
-                    } else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.Y)) {
-                        drive.turn(Math.toRadians(-180));
-                        turnState = TurnState.RIGHT;
                     }
                     break;
                 case RIGHT:
-                    LeftXInput = gamepad1.left_stick_y * leftStickMultiplierY * intakeSlide.dropOffMultiplier;
-                    LeftYInput = -gamepad1.left_stick_x * leftStickMultiplierX * intakeSlide.dropOffMultiplier;
-                    RightXInput = gamepad1.right_stick_x * rightStickMultiplierX * intakeSlide.dropOffMultiplier;
-                    if (gamepadEx1.wasJustReleased(GamepadKeys.Button.X)) {
-                        drive.turn(Math.toRadians(-180));
-                        turnState = TurnState.LEFT;
-                    } else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.Y)) {
+                    LeftXInput = gamepad1.left_stick_y * MaxSpeedMultiplier * intakeSlide.dropOffMultiplier;
+                    LeftYInput = -gamepad1.left_stick_x * MaxSpeedMultiplier * intakeSlide.dropOffMultiplier;
+                    RightXInput = gamepad1.right_stick_x * MaxSpeedMultiplier * intakeSlide.dropOffMultiplier;
+                    if (gamepadEx1.wasJustReleased(GamepadKeys.Button.DPAD_LEFT)) {
                         drive.turn(Math.toRadians(90));
                         turnState = TurnState.STRAIGHT;
+                    } else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.DPAD_RIGHT)) {
+                        drive.turn(Math.toRadians(-90));
+                        turnState = TurnState.BACKWARDS;
+                    }
+                    break;
+                case BACKWARDS:
+                    LeftXInput = -gamepad1.left_stick_x * MaxSpeedMultiplier * intakeSlide.dropOffMultiplier;
+                    LeftYInput = -gamepad1.left_stick_y * MaxSpeedMultiplier * intakeSlide.dropOffMultiplier;
+                    RightXInput = gamepad1.right_stick_x * MaxSpeedMultiplier * intakeSlide.dropOffMultiplier;
+                    if (gamepadEx1.wasJustReleased(GamepadKeys.Button.DPAD_LEFT)) {
+                        drive.turn(Math.toRadians(90));
+                        turnState = TurnState.RIGHT;
+                    } else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.DPAD_RIGHT)) {
+                        drive.turn(Math.toRadians(-90));
+                        turnState = TurnState.LEFT;
                     }
                     break;
             }
@@ -126,22 +125,38 @@ public class PowerPlayTeleOpServo extends LinearOpMode {
 //            LeftYInput = gamepad1.left_stick_y * leftStickMultiplierY * intakeSlide.dropOffMultiplier;
 //            RightXInput = gamepad1.right_stick_x * rightStickMultiplierX * intakeSlide.dropOffMultiplier;
 
-            drive.setWeightedDrivePower(
-                    new Pose2d(
-                            -LeftYInput,
-                            -LeftXInput,
-                            -RightXInput
-                    )
-            );
+            if (!sensorTouch1.isPressed() || !sensorTouch2.isPressed()) {
+                drive.setWeightedDrivePower(
+                        new Pose2d(
+                                -LeftYInput,
+                                -LeftXInput,
+                                -RightXInput
+                        )
+                );
+            } else if (sensorTouch1.isPressed() && sensorTouch1.isPressed()) {
+                drive.setWeightedDrivePower(
+                        new Pose2d(
+                                -0,
+                                -0.1,
+                                -0
+                        )
+                );
+            }
 
             drive.update();
 
-            alignStick.run();
-//            if (gamepadEx1.wasJustReleased(GamepadKeys.Button.X)) {
-//                alignStick.turnAlign(gamepadEx1,0.5);
-//            } else if (gamepadEx1.wasJustReleased(GamepadKeys.Button.B)) {
-//                alignStick.turnAlign(gamepadEx1,-0.5);
-//            }
+            if (gamepadEx1.wasJustPressed(GamepadKeys.Button.A)) {
+                MaxSpeedMultiplier += 0.1;
+            } else if (gamepadEx1.wasJustPressed(GamepadKeys.Button.B)) {
+                MaxSpeedMultiplier -= 0.1;
+            }
+            if (MaxSpeedMultiplier > 1) {
+                MaxSpeedMultiplier = 1;
+            } else if (MaxSpeedMultiplier < 0) {
+                MaxSpeedMultiplier = 0;
+            }
+
+//            alignStick.run();
 
 //            telemetry.addData("X", poseEstimate.getX());
 //            telemetry.addData("Y", poseEstimate.getY());
@@ -156,22 +171,24 @@ public class PowerPlayTeleOpServo extends LinearOpMode {
 //            telemetry.addData("Current Slide Position 1", intakeSlide.getCurrentSlidePosition());
             telemetry.addData("Current State 1", intakeSlide.getCurrentState());
 //            telemetry.addData("How many DpadUp?", intakeSlide.getDpadPressed());
-            telemetry.addData(intakeSlide.getCurrentCaption(), intakeSlide.getCurrentStatus());
             telemetry.addData("Current Control", intakeSlide);
 //            telemetry.addData("Is intake pressed", intakeSlide3.getIntakePressed());
             telemetry.addData("Rotation", turnState.name());
 
             // Distance
-            telemetry.addData("range", String.format("%.01f cm", alignStick.getDistanceReadingCM()));
-            telemetry.addData("range", String.format("%.01f mm", alignStick.getDistanceReadingMM()));
+//            telemetry.addData("range", String.format("%.01f cm", alignStick.getDistanceReadingCM()));
+//            telemetry.addData("range", String.format("%.01f mm", alignStick.getDistanceReadingMM()));
 //            telemetry.addData("Align State", alignStick.getAlignState());
 //            telemetry.addData("Light State", alignStick.getLightState());
 //            telemetry.addData("Align Multiplier", alignStick.getGameStickMultiplier());
 
             telemetry.addData("Servo Position", intakeSlide.getServoPosition());
             telemetry.addData("Intake State", intakeSlide.getIntakeState());
+            telemetry.addData("Current Max Speed", MaxSpeedMultiplier);
+
             // publish all the telemetry at once
             telemetry.update();
         }
     }
+
 }
